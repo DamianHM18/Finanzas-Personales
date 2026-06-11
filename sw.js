5199ba98e899789bd6a1ba9cfc782b8b804029da
DamianHM18 <edimonterrubio100@gmail.com>
2026-06-11 10:57:31 -0600

    Add basic Service Worker for installable app

---
+++ sw.js
@@ -0,0 +1,4 @@
+// Este es un Service Worker básico para que el navegador reconozca la app como instalable.
+self.addEventListener('fetch', (event) => {
+    // No hace nada por ahora, pero su existencia es obligatoria para la instalación
+});
